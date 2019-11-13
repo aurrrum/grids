98af06fa7ba10f8b1e0455d8817fc5f650684909
aurrrum <44933360+aurrrum@users.noreply.github.com>
2019-11-13 13:46:39 +0300

    ---

--- mobile-nav.js
+++ mobile-nav.js
@@ -3,7 +3,6 @@ var toggleButton = document.querySelector(".toggle-button");
 var mobileNav= document.querySelector(".mobile-nav");
 
 backdrop.addEventListener("click", function() {
-  // mobileNav.style.display = "none";
     mobileNav.classList.remove("open");
     backdrop.style.display = "none";
     setTimeout(function() {
@@ -12,10 +11,7 @@ backdrop.addEventListener("click", function() {
 });
 
 toggleButton.addEventListener("click", function() {
-  // mobileNav.style.display = "block";
-  // backdrop.style.display = "block";
   mobileNav.classList.add("open");
-    // this isneeded to animate elemet with display property defined
   backdrop.style.display = "block";
   setTimeout(function() {
     backdrop.classList.add("open");
